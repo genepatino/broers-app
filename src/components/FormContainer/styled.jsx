053f2e2b3c2b9000041ee1ace3a80a7ee5b9ea13
Genesis Patiño <62777013+genepatino@users.noreply.github.com>
2024-09-03 07:57:06 -0500

    fix styles login

--- src/components/FormContainer/styled.jsx
+++ src/components/FormContainer/styled.jsx
@@ -1,40 +1,41 @@
 import styled from "styled-components";
 
 export const SectionLogin = styled.section`
-    background-color: white;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 60px;
+  padding-top: 70px;
+  border-bottom-right-radius: 20px;
+  border-top-right-radius: 20px;
+
+  h1 {
+    font-size: 3rem;
+    margin-bottom: 40px;
+    letter-spacing: 0.22rem;
+    font-family: system-ui;
+  }
+  p {
+    font-size: 1.4rem;
+    font-family: system-ui;
+  }
+  .login-form {
+    width: 100%;
+    margin-top: 40px;
     display: flex;
     flex-direction: column;
-    align-items: center;
-    padding: 0 60px;
-    padding-top: 70px;
-    border-bottom-right-radius: 20px;
-    border-top-right-radius: 20px;
+    gap: 20px;
+  }
+  .label-item {
+    text-align: start;
+  }
+  .btn-submit {
+    background-color: #891212e3;
+    padding: 18px 32px;
+  }
 
-    h1{
-      font-size: 3rem;
-      margin-bottom: 14px;
-      letter-spacing: 0.22rem;
-      font-family: system-ui;
-    }
-    p{
-      font-size: 1.4rem;
-    }
-    .login-form{
-      width: 100%;
-      margin-top: 60px;
-      display: flex;
-      flex-direction: column;
-      gap: 20px;
-    }
-    .label-item{
-      text-align: start;
-    }
-    .btn-submit{
-      background-color: #891212e3;
-      padding: 18px 32px;
-    }
-
-    .btn-submit:hover{
-      background-color: #891212e3 !important
-    }
-`
+  .btn-submit:hover {
+    background-color: #891212e3 !important;
+  }
+`;
--- src/pages/Login/styled.jsx
+++ src/pages/Login/styled.jsx
@@ -5,23 +5,23 @@ export const LayoutContainer = styled.div`
   height: 100vh;
   display: grid;
   place-items: center;
-`
+`;
 export const DivContainer = styled.div`
   height: 700px;
   box-shadow: 1px -3px 20px 0px #cbaeae96;
-  border-radius: 14px;
+  border-radius: 20px;
   display: grid;
   grid-template-columns: 1.2fr 1.5fr;
 
   border-bottom-left-radius: 20px;
   border-top-left-radius: 20px;
 
-  .section-img--hero{
-    figure{
+  .section-img--hero {
+    figure {
       display: flex;
-    align-items: center;
-    justify-content: center;
-      img{
+      align-items: center;
+      justify-content: center;
+      img {
         object-fit: cover;
         width: 100%;
         height: 700px;
@@ -31,4 +31,4 @@ export const DivContainer = styled.div`
       }
     }
   }
-`
+`;
